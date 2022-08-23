87fa5380e57a3600ab493ab9247d615bf383f1aa
scgorthi <scgorthi@stu.naperville203.org>
2022-08-23 15:08:01 -0500

    learned bluej and took notes

---
+++ My First BlueJ Project/Chap1notes.java
@@ -0,0 +1,37 @@
+
+/**
+ * Sniya Gorthi
+ * 8/23/2022
+ * Chapter 1 Notes
+ * Class notes on basic concepts
+ * Who helped me: Mr. Hayes
+ *
+ */
+public class Chap1notes
+{
+
+    /**
+     * this is our code notes
+     */
+    public static void main(String[] args)
+    {
+        //basic data types
+        double number = 8.9,number2;
+        int x = 9,y,z;
+        boolean choice = false;
+        char letter = 'b';
+        float number3 = 8;
+        long n2 = 3;
+        short n3 = 7;
+        
+        //These are Classes and Objects
+        //Class   Variable    Memory       Constructor
+        //Name
+        Integer    n1 =       new          Integer(47);
+        String    word =      new          String("Sniya Gorthi");
+        Double    number4 =   new          Double(6.7);
+        
+        System.out.print(word.length());
+        
+    }
+}
